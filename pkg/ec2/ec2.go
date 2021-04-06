0e3a8417f839cf61f186f53d02eff104a94e942d
Cheuk Lau <cheuk.lau@aggienetwork.com>
2021-04-05 23:01:24 -0700

    Add owner based on tag of EC2 instance

--- pkg/ec2/ec2.go
+++ pkg/ec2/ec2.go
@@ -9,7 +9,7 @@ import (
 
 type Ec2 struct {
 	instanceType string
-	// todo: add more info including tag for owner
+	owner string
 }
 
 func Run(sess *session.Session) {
@@ -18,12 +18,12 @@ func Run(sess *session.Session) {
 	// Create EC2 session
 	svc := ec2.New(sess)
 
-	// Find all instances
-	// Note: Do not need to pass a map by reference
+	// Gather all instances and metadata
+	// Note: Do not pass a map by reference
 	allEc2s := make(map[string]Ec2)
 	FindAllEc2s(svc, allEc2s)
 	for instanceId, instanceMeta := range allEc2s {
-		fmt.Println(instanceId, instanceMeta.instanceType)
+		fmt.Println(instanceId, instanceMeta.instanceType, instanceMeta.owner)
 	}
 }
 
@@ -32,11 +32,20 @@ func FindAllEc2s(svc *ec2.EC2, allEc2s map[string]Ec2) {
 	if err != nil {
 		log.Fatal("Unable to retrieve EC2 instances. Error:", err)
 	}
+	var owner string
 	for _, reservation := range result.Reservations {
 		for _, instance := range reservation.Instances {
-			// todo: add more info including tag for owner
+			// Find the owner based on tag
+			owner = "unknown"
+			for _, tag := range instance.Tags {
+				if *tag.Key == "owner" {
+					owner = *tag.Value
+					break
+				}
+			}
 			allEc2s[*instance.InstanceId] = Ec2{
 				*instance.InstanceType,
+				owner,
 			}
 		}
 	}
