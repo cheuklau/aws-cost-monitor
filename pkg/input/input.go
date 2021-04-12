074ef4c85e36a2e07d67b7f585ada71cba92bba9
Cheuk Lau <cheuk.lau@aggienetwork.com>
2021-04-11 18:39:11 -0700

    Stupid spacing issues. I hate you

--- pkg/input/input.go
+++ pkg/input/input.go
@@ -1,22 +1,22 @@
 package input
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "gopkg.in/yaml.v2"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"gopkg.in/yaml.v2"
 )
 
 type RulesConfig struct {
-    Team string `yaml:"team"`
-    Ec2 struct {
+	Team string `yaml:"team"`
+	Ec2 struct {
 		Sizes []string `yaml:"sizes,flow"`
 		MaxCount []int `yaml:"maxCount,flow"`
 	}
 }
 
 func (c *RulesConfig) Parse(data []byte) error {
-    return yaml.Unmarshal(data, c)
+	return yaml.Unmarshal(data, c)
 }
 
 func Run(filename string) RulesConfig {
